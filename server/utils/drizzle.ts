52f6922503e6811cf4fe47f860a1cc50777c5959
madong <viaimperiale@gmail.com>
2024-10-27 22:05:29 +0100

    fix(drizzle): column names default to snake case

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -3,11 +3,9 @@ import * as schema from "../database/schema";
 import postgres from "postgres";
 import { useEnv } from "~/server/utils/env";
 
-export { sql, eq, and, or } from "drizzle-orm";
-
 export const tables = schema;
 const client = postgres(useEnv.NUXT_PROGRES_URL);
 
 export function useDrizzle() {
-  return drizzle(client, { schema });
+  return drizzle(client, { schema, casing: "snake_case" });
 }
