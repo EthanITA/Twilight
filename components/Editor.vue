d8255e97c494fdd9843c4d005f0c6d8457d1ed52
madong <viaimperiale@gmail.com>
2024-10-30 21:04:33 +0100

    build(notes): add tiptap

---
+++ components/Editor.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="text-2xl">hello</h1>
+  <editor-content :editor="editor" class="bg-gray-200" />
+</template>
+
+<script lang="ts" setup>
+import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from "@tiptap/vue-3";
+
+const modelValue = defineModel<string>({ default: "" });
+const editor = new Editor({
+  extensions: [StarterKit],
+  content: modelValue.value,
+  onUpdate: () => {
+    modelValue.value = editor.getHTML();
+  },
+});
+
+watch(modelValue, (value) => {
+  if (editor.getHTML() === value) return;
+  editor.commands.setContent(value, false);
+});
+onUnmounted(editor.destroy);
+</script>
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <div class="text-lg btn btn-primary btn-sm">
-    <h1>Home</h1>
-  </div>
-</template>
-
-<style scoped></style>
