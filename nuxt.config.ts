58cecc67228edcd6ac4635332c4082362f57b048
madong <viaimperiale@gmail.com>
2024-11-12 00:30:30 +0100

    feat(app): add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
         },
       ],
     },
+    pageTransition: { name: "page", mode: "out-in" },
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
