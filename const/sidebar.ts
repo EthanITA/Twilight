eb061c19bc0f1d70f8abbaaffdd15a9a7c405add
madong <viaimperiale@gmail.com>
2024-11-11 23:56:34 +0100

    refactor(sidebar): move apps to const

--- const/sidebar.ts
+++ const/sidebar.ts
@@ -1,4 +1,13 @@
-export const apps = [
+import {
+  BookOpenIcon,
+  CalendarDaysIcon,
+  CalendarIcon,
+  MapIcon,
+  RectangleGroupIcon,
+  RectangleStackIcon,
+} from "@heroicons/vue/24/solid";
+
+export const appNames = [
   "Notes",
   "Timeline",
   "Planner",
@@ -6,3 +15,43 @@ export const apps = [
   "Calendar",
   "Today",
 ] as const;
+type AppName = (typeof appNames)[number];
+
+interface AppLink {
+  name: AppName;
+  link: string;
+  icon: Component;
+}
+
+export const apps: AppLink[] = [
+  {
+    name: "Notes",
+    link: "/notes",
+    icon: BookOpenIcon,
+  },
+  {
+    name: "Timeline",
+    link: "/timeline",
+    icon: MapIcon,
+  },
+  {
+    name: "Planner",
+    link: "/planner",
+    icon: RectangleStackIcon,
+  },
+  {
+    name: "Board",
+    link: "/board",
+    icon: RectangleGroupIcon,
+  },
+  {
+    name: "Calendar",
+    link: "/calendar",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Today",
+    link: "/today",
+    icon: CalendarDaysIcon,
+  },
+];
--- store/sidebar.ts
+++ store/sidebar.ts
@@ -1,55 +1,6 @@
-import type { apps } from "~/const/sidebar";
-import {
-  BookOpenIcon,
-  CalendarDaysIcon,
-  CalendarIcon,
-  MapIcon,
-  RectangleGroupIcon,
-  RectangleStackIcon,
-} from "@heroicons/vue/24/solid";
-
-type AppName = (typeof apps)[number];
-
-interface AppLink {
-  name: AppName;
-  link: string;
-  icon: Component;
-}
+import { apps } from "~/const/sidebar";
 
 export const useSidebar = defineStore("sidebar", () => {
-  const apps: AppLink[] = [
-    {
-      name: "Notes",
-      link: "/notes",
-      icon: BookOpenIcon,
-    },
-    {
-      name: "Timeline",
-      link: "/timeline",
-      icon: MapIcon,
-    },
-    {
-      name: "Planner",
-      link: "/planner",
-      icon: RectangleStackIcon,
-    },
-    {
-      name: "Board",
-      link: "/board",
-      icon: RectangleGroupIcon,
-    },
-    {
-      name: "Calendar",
-      link: "/calendar",
-      icon: CalendarIcon,
-    },
-    {
-      name: "Today",
-      link: "/today",
-      icon: CalendarDaysIcon,
-    },
-  ];
-
   return {
     apps,
   };
